11ff2ac8a506deab016a07961ea7f1452a5e272a
Massimiliano Mirra <bard@hyperstruct.net>
2020-12-31 20:03:34 +0000

    add copyright notice

--- run-command.el
+++ run-command.el
@@ -1,3 +1,32 @@
+;;; run-command.el --- Run an external command from a list
+
+;; Copyright (C) 2020-2021 Massimiliano Mirra
+
+;; Author: Massimiliano Mirra <hyperstruct@gmail.com>
+;; URL: https://github.com/bard/emacs-run-command
+;; Version: 0.1.0
+;; Keywords: compilation
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
 
 (declare-function helm "ext:helm")
 (declare-function helm-build-sync-source "ext:helm")
