9bb89eb2965f72b718b9d56c9c9c71cd798cdc27
Massimiliano Mirra <bard@hyperstruct.net>
2021-01-01 10:26:20 +0000

    remove function for user-defined global scripts

--- run-command-global.el
+++
@@ -1,9 +0,0 @@
-
-(defun run-command-global ()
-  (list (list :display "clone node starter"
-              :name "clone-node-starter"
-              :command "git clone ~/projects/starter-node-basic"
-              :working-dir default-directory
-              :scope-name default-directory)))
-
-(provide 'run-command-global)
