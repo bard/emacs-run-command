7a960b08490a4c3c98dc41633df6510253a58d8b
Massimiliano Mirra <bard@hyperstruct.net>
2021-01-02 18:26:07 +0000

    move helm and ivy integration back to main file

--- run-command.el
+++ run-command.el
@@ -31,13 +31,11 @@
 
 ;;; Code:
 
-
 (declare-function helm "ext:helm")
 (declare-function helm-build-sync-source "ext:helm")
 (declare-function ivy-read "ext:ivy")
 
-(require 'run-command--helm)
-(require 'run-command--ivy)
+;; Customization
 
 (defgroup run-command nil
   "Run an external command from a context-dependent list."
@@ -54,6 +52,8 @@
   "List of functions that will produce runnable commands."
   :type '(repeat function):group'run-command)
 
+;; Entry point
+
 (defun run-command ()
   (interactive)
   (pcase run-command-completion-method
@@ -66,9 +66,62 @@
                      (run-command--ivy-targets)
                      :action 'run-command--ivy-action)))))
 
+;; Utilities
+
 (defun run-command--compilation-buffer-name (command-name scope-name)
   (lambda (mode-name)
     (format "*%s[%s]*" command-name scope-name)))
+
+;; Helm integration
+
+(defun run-command--helm-sources ()
+  (mapcar 'run-command--helm-source-from-config
+          run-command-config))
+
+(defun run-command--helm-source-from-config (command-list-generator)
+  (let* ((command-specs (funcall command-list-generator))
+         (candidates (mapcar (lambda (command-spec)
+                               (cons (plist-get command-spec :display) command-spec))
+                             command-specs)))
+    (helm-build-sync-source (symbol-name command-list-generator)
+      :action 'run-command--helm-action
+      :candidates candidates
+      :filtered-candidate-transformer '(helm-adaptive-sort))))
+
+(defun run-command--helm-action (command-spec)
+  (cl-destructuring-bind
+      (&key command-name command-line scope-name working-dir &allow-other-keys)
+      command-spec
+    (let ((compilation-buffer-name-function
+           (run-command--compilation-buffer-name command-name scope-name))
+          (default-directory working-dir)
+          (final-command-line (if helm-current-prefix-arg
+                                  (read-string "> " (concat command-line " "))
+                                command-line)))
+      (compile final-command-line))))
+
+;; Ivy integration
+
+(defun run-command--ivy-targets ()
+  (mapcan (lambda (command-list-generator)
+            (let ((command-specs (funcall command-list-generator)))
+              (mapcar (lambda (command-spec)
+                        (cons (concat (propertize (concat (symbol-name command-list-generator)
+                                                          "/")
+                                                  'face
+                                                  'shadow)
+                                      (plist-get command-spec :display)) command-spec))
+                      command-specs)))
+          run-command-config))
+
+(defun run-command--ivy-action (selection)
+  (cl-destructuring-bind
+      (&key command-name command-line scope-name working-dir &allow-other-keys)
+      (cdr selection)
+    (let ((compilation-buffer-name-function
+           (run-command--compilation-buffer-name command-name scope-name))
+          (default-directory working-dir))
+      (compile command-line))))
 
 (provide 'run-command)
 
