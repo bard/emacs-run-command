7b7046fee0eafcbc46abdfe55f05c4c97d7d39a2
Massimiliano Mirra <bard@hyperstruct.net>
2021-01-04 21:55:38 +0000

    move command recipe to main file

--- run-command.el
+++ run-command.el
@@ -99,6 +99,32 @@ said functions)."
                       (run-command--ivy-targets)
                       :action 'run-command--ivy-action)))))
 
+;; Recipes
+
+(defun run-command-package-json ()
+  (let ((project-dir (locate-dominating-file default-directory
+                                             "package.json")))
+    (when project-dir
+      (with-temp-buffer
+        (insert-file-contents (concat project-dir "package.json"))
+        (let* ((package-json (json-parse-buffer))
+               (project-name (gethash "name" package-json))
+               (script-map (gethash "scripts" package-json))
+               (scripts '())
+               (runner (if (file-exists-p (concat project-dir "yarn.lock"))
+                           "yarn"
+                         "npm")))
+          (maphash (lambda (key)
+                     (let ((command (concat runner " run " key)))
+                       (push (list :command-name key
+                                   :command-line command
+                                   :display key
+                                   :scope-name project-name
+                                   :working-dir project-dir)
+                             scripts)))
+                   script-map)
+          scripts)))))
+
 ;; Utilities
 
 (defun run-command--compilation-buffer-name (command-name scope-name)
