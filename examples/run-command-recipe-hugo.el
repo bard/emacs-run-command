e22d7abdc74233e598a7aef2150338ede1703f4d
Massimiliano Mirra <bard@hyperstruct.net>
2021-01-15 12:31:29 +0000

    enh: in example hugo recipe, read from `.env` if available

--- examples/run-command-recipe-hugo.el
+++ examples/run-command-recipe-hugo.el
@@ -1,16 +1,23 @@
 
 (defun run-command-recipe-hugo ()
-  (let* ((project-dir (locate-dominating-file default-directory
+  (let* ((dotenv-snippet (concat "set -a; test -f && source .env; set +a;")
+         (project-dir (locate-dominating-file default-directory
                                               "archetypes")))
     (when project-dir
-      (let ((project-name (file-name-nondirectory (directory-file-name project-dir))))
+      (let ((project-name (file-name-nondirectory
+                           (directory-file-name project-dir))))
         (list
-         (list :command-name "start:server"
-               :command-line "hugo server"
+         ;; Next two commands have the same `:command-name' and thus are mutually
+         ;; exclusive — running one will ask to stop the other first.
+         (list :command-name "hugo-server"
+               ;; Read from `.env' if available (e.g. for HUGO_MODULE_REPLACEMENTS)
+               :command-line (concat dotenv-snippet "hugo server")
                :display "Start local preview server"
-               :working-dir project-dir)
-         (list :command-name "start:server"
-               :command-line "hugo server --disableFastRender -D --navigateToChanged"
+               :working-dir project-dir)         
+         (list :command-name "hugo-server"
+               :command-line (concat
+                              dotenv-snippet
+                              "hugo server --disableFastRender -D --navigateToChanged")
                :display "Start local preview server (drafts+fastRender)"
                :working-dir project-dir)
          (list :command-name "build"
