94d1bfb2b97814dbd8db68e28e7a6ebff32298db
Massimiliano Mirra <bard@hyperstruct.net>
2021-01-15 12:31:07 +0000

    fix: use default dir as scope (as per default) in hugo example

--- examples/run-command-recipe-hugo.el
+++ examples/run-command-recipe-hugo.el
@@ -8,22 +8,18 @@
          (list :command-name "start:server"
                :command-line "hugo server"
                :display "Start local preview server"
-               :scope-name project-name
                :working-dir project-dir)
          (list :command-name "start:server"
                :command-line "hugo server --disableFastRender -D --navigateToChanged"
                :display "Start local preview server (drafts+fastRender)"
-               :scope-name project-name
                :working-dir project-dir)
          (list :command-name "build"
                :command-line "hugo"
                :display "Build"
-               :scope-name project-name
                :working-dir project-dir)
          (list :command-name "post:new"
                :command-line "hugo new posts/unnamed.md"
                :display "New post"
-               :scope-name project-name
                :working-dir project-dir))))))
 
 
