bea37603bad9b16aeef538e197c3b324f5f2ebbb
Massimiliano Mirra <bard@hyperstruct.net>
2021-02-04 10:57:25 +0000

    docs: update dir-locals experiment

--- examples/run-command-recipe-dir-locals.el
+++ examples/run-command-recipe-dir-locals.el
@@ -1,31 +1,35 @@
-;; EXPERIMENTAL. This example describes how to have per-directory
-;; commands. Useful for simple cases where you don't want to bring in
-;; a build tool or task runner such as Make or NPM.
-;;
-;; 1. Enable the static recipe experiment:
 
-(run-command--enable-experiments '(static-recipes))
+;; This example describes per-directory commands. Useful for simple
+;; cases where you don't want to bring in a build tool or task runner
+;; such as Make or NPM.
 
-;; 2. Define an empty static recipe (it will be filled in later):
+;; 1. In your init file, define a variable that will hold
+;; per-directory commands, and a recipe function that will return
+;; those commands when available:
 
-(defvar run-command-recipe-dir-locals nil)
+(defvar run-command-recipe-dir-locals-command-specs '())
 
-;; 3. Customize `run-command-recipes' so that it includes
+(defun run-command-recipe-dir-locals ()
+  run-command-recipe-dir-locals-command-specs)
+
+;; 2. Customize `run-command-recipes' to include
 ;; `run-command-recipe-dir-locals'.
-;;
-;; 3. Create a `.dir-locals.el' file in the directory where you want
-;; some commands activated, containing the following:
+
+;; 3. In the directory where you want some commands activated, create
+;; a `.dir-locals.el' file and add definitions such as (note the
+;; leading periods):
 
 ((nil
-  . ((run-command-recipe-dir-locals
+  . ((run-command-recipe-dir-locals-command-specs
       . (( :command-name "say-hello"
            :command-line "echo hello, world!"))))))
 
-;; (Note the leading periods.)
-;;
-;; This is limited to static recipes, i.e. those recipes that don't
-;; need to switch or modify commands dynamically.
-;;
-;; Read more about per-directory local variables:
+;; This is currently limited to static recipes, i.e. recipes that
+;; don't need to toggle or modify commands dynamically. Commands will
+;; run in the directory of the buffer from which they are called. More
+;; sophisticated behavior (such as substituing variable or selecting
+;; the working directory) can be attained by pre-processing commands
+;; inside the `run-command-recipe-dir-locals' function.  More about
+;; per-directory local variables:
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html
 
