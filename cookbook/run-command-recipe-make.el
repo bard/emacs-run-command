9d69aa973233552d34dbec1c51e12389eaaf4035
Massimiliano Mirra <bard@hyperstruct.net>
2023-02-04 09:53:03 +0000

    usab,enh: don't trigger error if helm-make is not available

--- cookbook/run-command-recipe-make.el
+++ cookbook/run-command-recipe-make.el
@@ -12,20 +12,23 @@
 (defun run-command-recipe-make ()
   "Provide commands to run Makefile targets.
 
-Requires `helm-make' (https://github.com/abo-abo/helm-make) to be installed."
-  (require 'helm-make)
-  (when-let* ((project-dir
-               (locate-dominating-file default-directory "Makefile"))
-              (makefile (concat project-dir "Makefile"))
-              (targets (helm--make-cached-targets makefile)))
-    (seq-map
-     (lambda (target)
-       (list
-        :command-name target
-        :command-line (concat "make " target)
-        :display target
-        :working-dir project-dir))
-     targets)))
+Requires `helm-make' (https://github.com/abo-abo/helm-make) to
+read Makefile targets, but does not require `helm' and can be
+used with any of the selectors supported by `run-command'."
+
+  (when (require 'helm-make nil t)
+    (when-let* ((project-dir
+                 (locate-dominating-file default-directory "Makefile"))
+                (makefile (concat project-dir "Makefile"))
+                (targets (helm--make-cached-targets makefile)))
+      (seq-map
+       (lambda (target)
+         (list
+          :command-name target
+          :command-line (concat "make " target)
+          :display target
+          :working-dir project-dir))
+       targets))))
 
 (provide 'run-command-recipe-make)
 ;;; run-command-recipe-make.el ends here
